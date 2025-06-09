1eb78ea69d0c8ed2a3e191ac68a8eb3fc7794742
Arancibia Leiria Leandro Nicanor <Nicanor95@users.noreply.github.com>
2025-06-09 19:05:47 -0300

    Completar menu()

--- Ejercicio14/src/ejercicio14/Ejercicio14.java
+++ Ejercicio14/src/ejercicio14/Ejercicio14.java
@@ -1,6 +1,9 @@
 package ejercicio14;
 
 import ejercicio14.lib.GrafoM;
+import ejercicio14.lib.VerticeCosto;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Ejercicio14 {
@@ -8,6 +11,8 @@ public class Ejercicio14 {
     public static void main(String[] args) {
         GrafoM redAgua = new GrafoM(10);
         Scanner scanner = new Scanner(System.in);
+        ArrayList costosEIntermedios = new ArrayList<>();
+        Boolean cambios = true;
         String input;
         
         do {
@@ -19,28 +24,92 @@ public class Ejercicio14 {
                     // Inicializar agrega vertices (bombas) de forma interactiva
                     // Amplia el arreglo si es necesario.
                     redAgua.inicializar();
+                    cambios = true;
                 }
                 case "2" -> {
-                    // Agregar canal
+                    System.out.print("Zona origen: ");
+                    String origen = scanner.nextLine();
+                    System.out.print("Zona destino: ");
+                    String destino = scanner.nextLine();
+                    System.out.print("Capacidad del canal: ");
+                    int capacidad = scanner.nextInt();
+                    scanner.nextLine(); // limpiar buffer
+                    try {
+                        redAgua.agregarArco(origen, destino, capacidad);
+                        System.out.println("Canal agregado con éxito.");
+                    } catch (UnsupportedOperationException e) {
+                        System.out.println("Error: " + e.getMessage());
+                    } finally {
+                        cambios = true;
+                    }
                 }
                 case "3" -> {
                     // Mostrar red
                     redAgua.dibujarMatriz(true);
+                    
+                    System.out.printf("%n%nPresione ENTER para continuar...%n");
+                    scanner.nextLine();
                 }
                 case "4" -> {
                     // Capacidad bomba a bomba
+                    System.out.print("Zona origen: ");
+                    String origen = scanner.nextLine();
+                    System.out.print("Zona destino: ");
+                    String destino = scanner.nextLine();
+                    
+                    System.out.printf("Flujo máximo: %d%n", redAgua.dijkstraInvertido(origen, destino));
+                    
+                    System.out.printf("%n%nPresione ENTER para continuar...%n");
+                    scanner.nextLine();
                 }
                 case "5" -> {
-                    // Capacidad hasta bomba?
+                    // Capacidad hasta bomba
+                    System.out.print("Zona origen: ");
+                    String origen = scanner.nextLine();
+                    
+                    // Calculamos el maximo flujo de todos los elementos a origen.
+                    LinkedList<VerticeCosto> flujos = redAgua.maximoFlujoA(origen);
+                    
+                    System.out.println("Capacidad maxima de cualquier bomba a la actual:");
+                    for (VerticeCosto x: flujos) {
+                        System.out.printf("|%10s:%-10d|%n", x.getVertice(), x.getCosto());
+                    }
+                    
+                    System.out.printf("%n%nPresione ENTER para continuar...%n");
+                    scanner.nextLine();
                 }
                 case "6" -> {
-                    // Camino capacidad maxima a bomba?
+                    // Camino capacidad maxima a bomba
+                    System.out.print("Zona origen: ");
+                    String origen = scanner.nextLine();
+                    System.out.print("Zona destino: ");
+                    String destino = scanner.nextLine();
+                    
+                    if (cambios) { //floydWarshall es O^3!!!
+                        costosEIntermedios = redAgua.floydWarshall();
+                        cambios = true;
+                    }
+                    
+                    // Obtenemos los intermedios del floydWarshall
+                    Integer[][] intermedios = (Integer[][]) costosEIntermedios.get(1);
+                    
+                    // Usamos los intermedios para construir el camino.
+                    ArrayList camino = redAgua.getCamino(intermedios, origen, destino);
+                    
+                    // Dibujamos el camino.
+                    System.out.println(String.join(" --> ", camino));
+                    
+                    
+                    System.out.printf("%n%nPresione ENTER para continuar...%n");
+                    scanner.nextLine();
                 }
                 case "7" -> {
                     System.exit(0);
                 }
                 default -> {
                     System.out.println("Ingreso incorrecto.");
+                    System.out.printf("%n%nPresione ENTER para continuar...%n");
+                    scanner.nextLine();
                 }
             }
         } while (!input.startsWith("7"));
