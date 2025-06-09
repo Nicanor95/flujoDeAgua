cff8294fba8912958387496859c18d094719bfab
Arancibia Leiria Leandro Nicanor <Nicanor95@users.noreply.github.com>
2025-06-09 16:51:03 -0300

    Add switch to main for menu

--- Ejercicio14/src/ejercicio14/Ejercicio14.java
+++ Ejercicio14/src/ejercicio14/Ejercicio14.java
@@ -1,16 +1,48 @@
 package ejercicio14;
 
+import ejercicio14.lib.GrafoM;
 import java.util.Scanner;
 
 public class Ejercicio14 {
     
     public static void main(String[] args) {
+        GrafoM redAgua = new GrafoM(10);
         Scanner scanner = new Scanner(System.in);
         String input;
         
         do {
             makeMenu();
             input = scanner.nextLine();
+            
+            switch (input) {
+                case "1" -> {
+                    // Inicializar agrega vertices (bombas) de forma interactiva
+                    // Amplia el arreglo si es necesario.
+                    redAgua.inicializar();
+                }
+                case "2" -> {
+                    // Agregar canal
+                }
+                case "3" -> {
+                    // Mostrar red
+                    redAgua.dibujarMatriz(true);
+                }
+                case "4" -> {
+                    // Capacidad bomba a bomba
+                }
+                case "5" -> {
+                    // Capacidad hasta bomba?
+                }
+                case "6" -> {
+                    // Camino capacidad maxima a bomba?
+                }
+                case "7" -> {
+                    System.exit(0);
+                }
+                default -> {
+                    System.out.println("Ingreso incorrecto.");
+                }
+            }
         } while (!input.startsWith("7"));
     }
     
